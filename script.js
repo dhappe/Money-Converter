47bd6e8aa0358a76a963f948f156d5ddbc059689
dhappe <dhappedevpe@gmail.com>
2023-09-25 17:42:40 -0300

    Initial Files

---
+++ script.js
@@ -0,0 +1,11 @@
+const convertButton = document.querySelector(".converter-buuton");
+
+function convertValues() {
+    const inputCurrencyValue = document.querySelector(".input-currency").value;
+
+    const dolarToday = 5.2;
+
+    const convertedValue = inputCurrencyValue / dolarToday;
+}
+
+converterButton.addEventlistener("click");
